c896d1d16d5d510a5e65ffe018bf7cec6d2e47e2
satorun <satorun.org@gmail.com>
2023-07-22 14:36:11 +0900

    Initial Commit

---
+++ TCATutorials/TCATutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialsApp.swift
+//  TCATutorials
+//
+//  Created by satorun on 2023/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
