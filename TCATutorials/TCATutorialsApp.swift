d63bb071b4fbf11ed7556065cb64adace2736b67
satorun <satorun.org@gmail.com>
2023-07-23 16:49:08 +0900

    section 3

--- TCATutorials/TCATutorialsApp.swift
+++ TCATutorials/TCATutorialsApp.swift
@@ -5,13 +5,18 @@
 //  Created by satorun on 2023/07/22.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCATutorialsApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: TCATutorialsApp.store)
         }
     }
 }
